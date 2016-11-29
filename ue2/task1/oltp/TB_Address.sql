6a321f387a3625b19670586552db5942e28bc57d
geoHeil <e1225063@student.tuwien.ac.at>
2016-11-29 14:30:45 +0100

    rm

--- ue2/task1/oltp/TB_Address.sql
+++ ue2/task1/oltp/TB_Address.sql
@@ -4,8 +4,6 @@ CREATE TABLE BI_OLTP_4.TB_Address
   AddressLine   VARCHAR(200) NOT NULL,
   City          VARCHAR(200) NOT NULL,
   StateProvince VARCHAR(200),
-  Suffix        VARCHAR(200),
-  EmailAddress  VARCHAR(200),
   PostalCode    VARCHAR(100) NOT NULL,
   CountryRegion VARCHAR(200),
   CONSTRAINT PK_Address PRIMARY KEY (AddressID)
