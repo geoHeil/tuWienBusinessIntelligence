ae9b2d2abc47bfe34873b2b259972431e92d7876
geoHeil <e1225063@student.tuwien.ac.at>
2016-11-29 13:06:21 +0100

    add tables

---
+++ ue2/task1/oltp/TB_CustomerAddress.sql
@@ -0,0 +1,9 @@
+CREATE TABLE BI_OLTP_4.TB_CustomerAddress
+(
+  CustomerID  INT,
+  AddressID   INT,
+  AddressType VARCHAR(100),
+  CONSTRAINT FK_Customer FOREIGN KEY (CustomerID) REFERENCES TB_Customer (CustomerID),
+  CONSTRAINT FK_Address FOREIGN KEY (AddressID) REFERENCES TB_Address (AddressID),
+  CONSTRAINT PK_CustomerAddress PRIMARY KEY (CustomerID, AddressID)
+);
