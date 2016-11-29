7442655941984ef26512c0b85353596d3556d676
Stefan Beyer <home@sbeyer.at>
2016-11-29 13:12:01 +0100

    create scripts hinzugefügt

---
+++ ue2/task1/oltp/TB_ProductCategory.sql
@@ -0,0 +1,8 @@
+CREATE TABLE BI_OLTP_4.TB_ProductCategory (
+	ProductCategoryID INT AUTO_INCREMENT,
+	ParentProductCategoryID INT,
+	Name NOT NULL,
+	
+	FOREIGN KEY FK_ParentProductCategory (ParentProductCategoryID) REFERENCES TB_ProductCategory (ProductCategoryID),
+	PRIMARY KEY PK_ProductCategory (ProductCategoryID),
+);
