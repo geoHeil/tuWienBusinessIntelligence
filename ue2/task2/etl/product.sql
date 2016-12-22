cc1a96eb4b1ed3a10a61ae45bf1ede9fbe3cb0b2
Stefan Beyer <home@sbeyer.at>
2016-12-22 11:20:50 +0100

    Änderungen am Stern-Schema

--- ue2/task2/etl/product.sql
+++ ue2/task2/etl/product.sql
@@ -1,5 +1,25 @@
 INSERT INTO BI_OLAP_4.DM_Product
-    SELECT ProductID, ProductNumber, Name, ProductModelName, StandardCost, ListPrice, ProductCategoryID as ProductSubCategory,
-      (SELECT ParentProductCategoryID from BI_OLTP_4.TB_ProductCategory WHERE ProductCategoryID = p.ProductCategoryID)
-    as ProductTopCategory, SellStartDate, SellEndDate, DiscontinuedDate, Size, Weight, ((SELECT ParentProductCategoryID from BI_OLTP_4.TB_ProductCategory WHERE ProductCategoryID = p.ProductCategoryID) = 1 OR ProductCategoryID IN (16, 18, 20)) as IsBulkyItem
-FROm BI_OLTP_4.TB_Product p
+  SELECT
+    ProductID,
+    ProductNumber,
+    Name,
+    ProductModelName,
+    StandardCost,
+    ListPrice,
+    (SELECT Name
+     FROM BI_OLTP_4.TB_ProductCategory
+     WHERE ProductCategoryID = p.ProductCategoryID)                                            AS ProductSubCategory,
+    (SELECT p3.Name
+     FROM BI_OLTP_4.TB_ProductCategory p2
+       JOIN BI_OLTP_4.TB_ProductCategory p3 ON p2.ParentProductCategoryID = p3.ProductCategoryID
+     WHERE p2.ProductCategoryID = p.ProductCategoryID)
+                                                                                               AS ProductTopCategory,
+    SellStartDate,
+    SellEndDate,
+    DiscontinuedDate,
+    Size,
+    Weight,
+    ((SELECT ParentProductCategoryID
+      FROM BI_OLTP_4.TB_ProductCategory
+      WHERE ProductCategoryID = p.ProductCategoryID) = 1 OR ProductCategoryID IN (16, 18, 20)) AS IsBulkyItem
+  FROM BI_OLTP_4.TB_Product p
