f3af534b25b6615ed9b1b7db1391cb5f3a20b185
geoHeil <georg.kf.heiler@gmail.com>
2016-12-23 16:25:43 +0100

    fixed some details

--- ue2/task3/C1.sql
+++ ue2/task3/C1.sql
@@ -3,10 +3,10 @@ SELECT
   CountryRegion "Country Region",
   cast(sum(IsLateShipment) as Numeric(8,2))*100 / cast(count(IsLateShipment) as Numeric(8,2)) AS "Percentage of Late Shipments"
 FROM
-  BI_USER.DW_FactSales,
-  BI_USER.DW_Location
+  BI_OLAP_4.DW_FactSales,
+  BI_OLAP_4.DW_Location
 WHERE
-  BI_USER.DW_FactSales.FK_ShipToAddressID = AddressID
+  BI_OLAP_4.DW_FactSales.FK_ShipToAddressID = AddressID
 ORDER BY
   CountryRegion,
   ShipmentMethod ASC
--- ue2/task3/D1.sql
+++ ue2/task3/D1.sql
@@ -6,7 +6,7 @@ FROM
   DW_Time
 WHERE
   BI_OLAP_4.DW_FactSales.FK_OrderDate = DW_Time."Date"
-    AND BI_OLAP_4.DW_FactSales.FK_OrderDate between cast('2004-01-01' AS date) AND cast('2004-07-31' AS date)
+    AND BI_OLAP_4.DW_FactSales.FK_OrderDate between cast('2014-01-01' AS date) AND cast('2014-07-31' AS date)
 ORDER BY
   MonthNumberOfYear
 GROUP BY
